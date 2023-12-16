75574dffb2fae1d0433c8e6e7ae3ecf9a44fcf5f
771-8bit <kazuki.fujita8823@gmail.com>
2023-12-16 22:36:02 +0900

    Create Serial_PIO.ino

---
+++ misc/Serial_PIO/Serial_PIO.ino
@@ -0,0 +1,18 @@
+SerialPIO mySerial(D0,D1);
+
+void setup() {
+  Serial.begin(115200);
+  mySerial.begin(115200);
+}
+
+void loop() {
+  if (Serial.available()) {
+    char c = (char)Serial.read();
+    mySerial.write(c);
+  }
+
+  if (mySerial.available()) {
+    char c = (char)mySerial.read();
+    Serial.write(c);
+  }
+}
