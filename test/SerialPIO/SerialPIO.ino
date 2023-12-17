ec1d9242e885b481cfbe141e687b60845899b9fd
771-8bit <kazuki.fujita8823@gmail.com>
2023-12-18 00:55:13 +0900

    Update SerialPIO.ino

--- test/SerialPIO/SerialPIO.ino
+++ test/SerialPIO/SerialPIO.ino
@@ -1,9 +1,9 @@
-SerialPIO mySerial(D0,D1,128); //txpin, rxpin, fifosize
+SerialPIO mySerial(D0, D1, 128); //txpin, rxpin, fifosize
 
 void setup() {
   Serial.begin(115200);
-  mySerial.begin(115200);
-  }
+    mySerial.begin(115200);
+}
 
 void loop() {
   if (Serial.available()) {
