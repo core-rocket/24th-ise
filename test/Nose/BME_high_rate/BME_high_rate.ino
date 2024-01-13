cc9d76956bc909ee5000812337a224e617892180
nihinihikun <107825036+nihinihikun@users.noreply.github.com>
2024-01-13 11:42:36 +0900

    delete missing spaces

--- test/Nose/BME_high_rate/BME_high_rate.ino
+++ test/Nose/BME_high_rate/BME_high_rate.ino
@@ -4,45 +4,45 @@
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
-Adafruit_BME280 bme; 
+Adafruit_BME280 bme;
 
 unsigned long delayTime;
-const int clockFrequency = 400000;//I2C bus speed
+const int clockFrequency = 400000;  //I2C bus speed
 
 void setup() {
-    Serial.begin(115200);
-    unsigned status;
-    Wire.setSDA(6);
-    Wire.setSCL(7);
-    Wire.setClock(clockFrequency);
-    Wire.begin();
-    status = bme.begin(0x76);
+  Serial.begin(115200);
+  unsigned status;
+  Wire.setSDA(6);
+  Wire.setSCL(7);
+  Wire.setClock(clockFrequency);
+  Wire.begin();
+  status = bme.begin(0x76);
 }
 
 
-void loop() { 
-    printValues();
-    delay(5);
+void loop() {
+  printValues();
+  delay(5);
 }
 
 
 void printValues() {
-    Serial.print("Temperature = ");
-    Serial.print(bme.readTemperature());
-    Serial.println(" °C");
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" °C");
 
-    Serial.print("Pressure = ");
+  Serial.print("Pressure = ");
 
-    Serial.print(bme.readPressure() / 100.0F);
-    Serial.println(" hPa");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println(" hPa");
 
-    Serial.print("Approx. Altitude = ");
-    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-    Serial.println(" m");
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
 
-    Serial.print("Humidity = ");
-    Serial.print(bme.readHumidity());
-    Serial.println(" %");
+  Serial.print("Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %");
 
-    Serial.println();
+  Serial.println();
 }
--- test/Nose/bme280test/bme280test.ino
+++ test/Nose/bme280test/bme280test.ino
@@ -4,43 +4,43 @@
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
-Adafruit_BME280 bme; 
+Adafruit_BME280 bme;
 
 unsigned long delayTime;
 
 void setup() {
-    Serial.begin(9600);
-    unsigned status;
-    Wire.setSDA(6);
-    Wire.setSCL(7);
-    Wire.begin();
-    status = bme.begin(0x76);
+  Serial.begin(9600);
+  unsigned status;
+  Wire.setSDA(6);
+  Wire.setSCL(7);
+  Wire.begin();
+  status = bme.begin(0x76);
 }
 
 
-void loop() { 
-    printValues();
-    delay(1000);
+void loop() {
+  printValues();
+  delay(1000);
 }
 
 
 void printValues() {
-    Serial.print("Temperature = ");
-    Serial.print(bme.readTemperature());
-    Serial.println(" °C");
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" °C");
 
-    Serial.print("Pressure = ");
+  Serial.print("Pressure = ");
 
-    Serial.print(bme.readPressure() / 100.0F);
-    Serial.println(" hPa");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println(" hPa");
 
-    Serial.print("Approx. Altitude = ");
-    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-    Serial.println(" m");
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
 
-    Serial.print("Humidity = ");
-    Serial.print(bme.readHumidity());
-    Serial.println(" %");
+  Serial.print("Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %");
 
-    Serial.println();
+  Serial.println();
 }
