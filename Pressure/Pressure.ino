95751b63078b2b12fa9e8b6e34dddb6cedb9b353
771-8bit <kazuki.fujita8823@gmail.com>
2024-02-25 00:42:25 +0900

    Update Pressure.ino

--- Pressure/Pressure.ino
+++ Pressure/Pressure.ino
@@ -1,6 +1,9 @@
 #include <SPI.h>
 #include <string.h>
 #include <CCP_MCP2515.h>
+#include <Adafruit_LPS35HW.h>
+
+Adafruit_LPS35HW lps35hw = Adafruit_LPS35HW();
 
 #define CAN_INT D6
 #define CAN_CS D7
@@ -17,10 +20,21 @@ void setup() {
 
   // CAN
   CCP.begin();
+
+  if (!lps35hw.begin_I2C()) {
+  //if (!lps35hw.begin_SPI(LPS_CS)) {
+  //if (!lps35hw.begin_SPI(LPS_CS, LPS_SCK, LPS_MISO, LPS_MOSI)) {
+    Serial.println("Couldn't find LPS35HW chip");
+    while (1);
+  }
+  lps35hw.setDataRate(LPS35HW_RATE_75_HZ);
 }
 
 void loop() {
-  // CCP.float_to_device(CCP_ID, 123.45);
+  CCP.float_to_device(0x82, lps35hw.readTemperature());
+  CCP.float_to_device(0x80, lps35hw.readTemperature());
+  CCP.float_to_device(0x81, lps35hw.readPressure());
+  delay(10);
 
   if (!digitalRead(CAN_INT))  // データ受信確認
   {
