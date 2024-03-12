02e3f5651531bc9904079b03989505d7fe47fc69
nihinihikun <107825036+nihinihikun@users.noreply.github.com>
2024-03-12 13:13:50 +0900

    Update E220_can.ino

--- test/E220_can/E220_can.ino
+++ test/E220_can/E220_can.ino
@@ -17,6 +17,8 @@ CCP_MCP2515 CCP(CAN0_CS, CAN0_INT);  //CAN
 - power: 13dBm
 - SF: TBD
 */  
+
+
 union uinonfloat{
   float f;
   byte b[4];
@@ -47,6 +49,7 @@ void loop() {
   if(send_allowed==true && payload_semapho==false){
     e220.TransmissionData(tx_payload);
     latest_send_time = millis();//送信済みの時間を記録
+    send_allowed = false;
   }
   
   delay(2000);
@@ -55,6 +58,7 @@ void loop() {
 void GeneratePayload(byte* tx_payload,bool _payload_semapho){
   _payload_semapho = true;
   unionuint32 mcutime_ms;
+  uinonfloat buf;
   CCP.read_device();
   mcutime_ms.i = millis();
   byte status_byte = 0x00;
@@ -100,76 +104,67 @@ void GeneratePayload(byte* tx_payload,bool _payload_semapho){
 
           break;
         case CCP_nose_temperature:
-          uninonfloat temperature;
-          temperature.f = CCP.float_from_device();
-          tx_payload[11] = temperature.b[0];
-          tx_payload[12] = temperature.b[1];
-          tx_payload[13] = temperature.b[2];
-          tx_payload[14] = temperature.b[3];
+          buf.f = CCP.data_float();
+          tx_payload[11] = buf.b[0];
+          tx_payload[12] = buf.b[1];
+          tx_payload[13] = buf.b[2];
+          tx_payload[14] = buf.b[3];
           break;
         case CCP_nose_barometic_pressure:
-          uninonfloat barometic_pressure;
-          barometic_pressure.f = CCP.float_from_device();
-          tx_payload[15] = barometic_pressure.b[0];
-          tx_payload[16] = barometic_pressure.b[1];
-          tx_payload[17] = barometic_pressure.b[2];
-          tx_payload[18] = barometic_pressure.b[3];
+          buf.f = CCP.data_float();
+          tx_payload[15] = buf.b[0];
+          tx_payload[16] = buf.b[1];
+          tx_payload[17] = buf.b[2];
+          tx_payload[18] = buf.b[3];
           break;
         case CCP_nose_voltage:
-          uninonfloat voltage;
-          voltage.f = CCP.float_from_device();
-          tx_payload[19] = voltage.b[0];
-          tx_payload[20] = voltage.b[1];
-          tx_payload[21] = voltage.b[2];
-          tx_payload[22] = voltage.b[3];
+          buf.f = CCP.data_float();
+          tx_payload[19] = buf.b[0];
+          tx_payload[20] = buf.b[1];
+          tx_payload[21] = buf.b[2];
+          tx_payload[22] = buf.b[3];
           break;
         case CCP_surface_pressure1_pressure_pa:
-          uninonfloat surface_pressure1;
-          surface_pressure1.f = CCP.float_from_device();
-          tx_payload[23] = surface_pressure1.b[0];
-          tx_payload[24] = surface_pressure1.b[1];
-          tx_payload[25] = surface_pressure1.b[2];
-          tx_payload[26] = surface_pressure1.b[3];          
+          buf.f = CCP.data_float();
+          tx_payload[23] = buf.b[0];
+          tx_payload[24] = buf.b[1];
+          tx_payload[25] = buf.b[2];
+          tx_payload[26] = buf.b[3];          
           break;
         case CCP_surface_pressure2_pressure_pa:
-          uninonfloat surface_pressure2;
-          surface_pressure2.f = CCP.float_from_device();
-          tx_payload[27] = surface_pressure2.b[0];
-          tx_payload[28] = surface_pressure2.b[1];
-          tx_payload[29] = surface_pressure2.b[2];
-          tx_payload[30] = surface_pressure2.b[3];
+          buf.f = CCP.data_float();
+          tx_payload[27] = buf.b[0];
+          tx_payload[28] = buf.b[1];
+          tx_payload[29] = buf.b[2];
+          tx_payload[30] = buf.b[3];
           break;
         case CCP_surface_pressure3_pressure_pa:
-          uninonfloat surface_pressure3;
-          surface_pressure3.f = CCP.float_from_device();
-          tx_payload[31] = surface_pressure3.b[0];
-          tx_payload[32] = surface_pressure3.b[1];
-          tx_payload[33] = surface_pressure3.b[2];
-          tx_payload[34] = surface_pressure3.b[3];
+          buf.f = CCP.data_float();
+          tx_payload[31] = buf.b[0];
+          tx_payload[32] = buf.b[1];
+          tx_payload[33] = buf.b[2];
+          tx_payload[34] = buf.b[3];
           break;
         case CCP_surface_pressure4_pressure_pa:
-          uninonfloat surface_pressure4;
-          surface_pressure4.f = CCP.float_from_device();
-          tx_payload[35] = surface_pressure4.b[0];
-          tx_payload[36] = surface_pressure4.b[1];
-          tx_payload[37] = surface_pressure4.b[2];
-          tx_payload[38] = surface_pressure4.b[3];
+          buf.f = CCP.data_float();
+          tx_payload[35] = buf.b[0];
+          tx_payload[36] = buf.b[1];
+          tx_payload[37] = buf.b[2];
+          tx_payload[38] = buf.b[3];
           break;
         case CCP_surface_pressure5_pressure_pa:
-          uninonfloat surface_pressure5;
-          surface_pressure5.f = CCP.float_from_device();
-          tx_payload[39] = surface_pressure5.b[0];
-          tx_payload[40] = surface_pressure5.b[1];
-          tx_payload[41] = surface_pressure5.b[2];
-          tx_payload[42] = surface_pressure5.b[3];
+          buf.f = CCP.data_float();
+          tx_payload[39] = buf.b[0];
+          tx_payload[40] = buf.b[1];
+          tx_payload[41] = buf.b[2];
+          tx_payload[42] = buf.b[3];
           break;
         case CCP_surface_pressure6_pressure_pa:
-          uninonfloat surface_pressure6;
-          surface_pressure6.f = CCP.float_from_device();
-          tx_payload[43] = surface_pressure6.b[0];
-          tx_payload[44] = surface_pressure6.b[1];
-          tx_payload[45] = surface_pressure6.b[2];
-          tx_payload[46] = surface_pressure6.b[3];       
+          buf.f = CCP.data_float();
+          tx_payload[43] = buf.b[0];
+          tx_payload[44] = buf.b[1];
+          tx_payload[45] = buf.b[2];
+          tx_payload[46] = buf.b[3];       
           break;
         default:
           break;
